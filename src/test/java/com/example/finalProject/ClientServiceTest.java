9884fc497db5e545abcb9824ef730919d7b79f2d
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-04-17 22:23:41 -0500

    Client test finished

--- src/test/java/com/example/finalProject/ClientServiceTest.java
+++ src/test/java/com/example/finalProject/ClientServiceTest.java
@@ -1,4 +1,106 @@
 package com.example.finalProject;
 
+import com.example.finalProject.exception.DataAlreadyExistsException;
+import com.example.finalProject.exception.DataNotFoundException;
+import com.example.finalProject.exception.InvalidDataException;
+import com.example.finalProject.model.Client;
+import com.example.finalProject.model.Employee;
+import com.example.finalProject.repository.ClientRepository;
+import com.example.finalProject.service.ClientService;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Optional;
+import static org.junit.Assert.assertTrue;
+
+import static org.mockito.Mockito.*;
+
 public class ClientServiceTest {
+    ClientRepository clientRepository;
+    ClientService clientService;
+
+    @Before
+    public void setUp(){
+        this.clientRepository=mock(ClientRepository.class);
+        this.clientService= new ClientService(clientRepository);
+    }
+
+    @Test(expected = InvalidDataException.class)
+    public void Should_ThrowException_When_IdNull(){
+        Client client = new Client(null,"Sofia","Millan",123123L,"isabella@gmail.com",
+                "Cll26","Medellín");
+        Client createdClient = this.clientService.addClient(client);
+        verify(clientRepository.save(createdClient));
+    }
+
+    @Test(expected = InvalidDataException.class)
+    public void Should_ThrowException_When_IdNotValid(){
+        Client client = new Client(12345678910L,"Sofia","Millan",123123L,"isabella@gmail.com",
+                "Cll26","Medellín");
+        Client createdClient = this.clientService.addClient(client);
+        verify(clientRepository.save(createdClient));
+    }
+
+    @Test(expected = InvalidDataException.class)
+    public void Should_ThrowException_When_EmailNotValid(){
+        Client client = new Client(123L,"Sofia","Millan",123123L,"isabellagmail.com",
+                "Cll26","Medellín");
+        Client createdClient = this.clientService.addClient(client);
+        verify(clientRepository.save(createdClient));
+    }
+
+    @Test(expected = DataNotFoundException.class)
+    public void Should_ThrowException_When_IdNotFoundGet(){
+        Long id = 123L;
+        when(clientRepository.findById(id)).thenReturn(Optional.empty());
+
+        Client client = this.clientService.getClient(id);
+
+       verify(clientRepository.findById(client.getId()));
+    }
+
+    @Test(expected = DataNotFoundException.class)
+    public void Should_ThrowException_When_IdNotFoundUpdate(){
+        Long id = 123L;
+        when(clientRepository.findById(id)).thenReturn(Optional.empty());
+        Client client = this.clientService.updateClient(id, new Client(123L,"Sofia","Millan",123123L,"isabellagmail.com",
+                "Cll26","Medellín") );
+        verify(clientRepository.save(client));
+    }
+
+    @Test(expected = DataAlreadyExistsException.class)
+    public void Should_ThrowException_When_IdExists(){
+
+        Client client = new Client(1L,"Sofia","Millan",123123L,"isabella@gmail.com",
+                "Cll26","Medellín");
+        Client client2 = new Client(1L,"Sofia","Millan",123123L,"isabella@gmail.com",
+                "Cll26","Medellín");
+
+        when(clientRepository.save(client2)).thenThrow(DataAlreadyExistsException.class);
+
+        Client createdClient1 = this.clientService.addClient(client);
+        Client createdClient2 = this.clientService.addClient(client2);
+
+        verify(clientRepository.save(createdClient2));
+    }
+
+    @Test
+    public void GetClient_FoundClient(){
+        Client client = new Client(123L,"Sofia","Millan",123123L,"isabella@gmail.com",
+                "Cll26","Medellín");
+
+         when(clientRepository.findById(client.getId())).thenReturn(Optional.of(client));
+
+         Optional<Client> optionalClient = clientRepository.findById(client.getId());
+
+        assertTrue(optionalClient.isPresent());
+    }
+
+
+    @Test
+    public void CreateClientTest(){
+        Client client = new Client(1L,"Sofia","Millan",123123L,"isabella@gmail.com",
+                "Cll26","Medellín");
+        Client createdClient = this.clientService.addClient(client);
+        verify(clientRepository).save(client);
+    }
 }
--- src/test/java/com/example/finalProject/EmployeeServiceTest.java
+++ src/test/java/com/example/finalProject/EmployeeServiceTest.java
@@ -1,18 +1,87 @@
 package com.example.finalProject;
 
-import com.example.finalProject.repository.ClientRepository;
-import com.example.finalProject.service.ClientService;
-import org.junit.Before;
+import com.example.finalProject.exception.DataAlreadyExistsException;
+import com.example.finalProject.exception.InvalidDataException;
 
-import static org.mockito.Mockito.mock;
+import com.example.finalProject.model.Client;
+import com.example.finalProject.repository.EmployeeRepository;
+
+import com.example.finalProject.model.Employee;
+import com.example.finalProject.service.EmployeeService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class EmployeeServiceTest {
-    ClientRepository clientRepository;
-    ClientService  clientService;
+    EmployeeRepository employeeRepository;
+    EmployeeService employeeService;
 
     @Before
     public void setUp(){
-        this.clientRepository=mock(ClientRepository.class);
-        this.clientService= new ClientService(clientRepository);
+        this.employeeRepository=mock(EmployeeRepository.class);
+        this.employeeService= new EmployeeService(employeeRepository);
     }
+
+    @Test(expected = InvalidDataException.class)
+    public void Should_ThrowException_When_IdNull(){
+        Employee employee = new Employee(null,"Sofia","Millan",123123L,"isabella@gmail.com",
+                "Cll26","Medellín",1,"o+","coordinator");
+        Employee createdEmployee = this.employeeService.addEmployee(employee);
+        verify(employeeRepository.save(createdEmployee));
+    }
+    @Test(expected = InvalidDataException.class)
+    public void Should_ThrowException_When_IdNotValid(){
+        Employee employee = new Employee(12345678912L,"Sofia","Millan",123123L,"isabella@gmail.com",
+                "Cll26","Medellín",1,"o+","coordinator");
+        Employee createdEmployee = this.employeeService.addEmployee(employee);
+        verify(employeeRepository.save(createdEmployee));
+    }
+
+    @Test(expected = InvalidDataException.class)
+    public void Should_ThrowException_When_EmployeeTypeNull(){
+        Employee employee = new Employee(1L,"Sofia","Millan",123123L,"isabella@gmail.com",
+                "Cll26","Medellín",1,"o+",null);
+        Employee createdEmployee = this.employeeService.addEmployee(employee);
+        verify(employeeRepository.save(createdEmployee));
+    }
+    @Test(expected = InvalidDataException.class)
+    public void Should_ThrowException_When_EmployeeTypeInvalid(){
+        Employee employee = new Employee(1L,"Sofia","Millan",123123L,"isabella@gmail.com",
+                "Cll26","Medellín",1,"o+","Customer Service");
+        Employee createdEmployee = this.employeeService.addEmployee(employee);
+        verify(employeeRepository.save(createdEmployee));
+    }
+
+    @Test(expected = InvalidDataException.class)
+    public void Should_ThrowException_When_EmailNotValid(){
+        Employee employee = new Employee(1L,"Sofia","Millan",123123L,"isabellagmail.com",
+                "Cll26","Medellín",1,"o+","coordinator");
+        Employee createdEmployee = this.employeeService.addEmployee(employee);
+        verify(employeeRepository.save(createdEmployee));
+    }
+
+    @Test
+    public void GetEmployee_FoundClient(){
+        Employee employee = new Employee(1L,"Sofia","Millan",123123L,"isabellagmail.com",
+                "Cll26","Medellín",1,"o+","coordinator");
+
+        when(employeeRepository.findById(employee.getId())).thenReturn(Optional.of(employee));
+
+        Optional<Employee> optionalEmployee = employeeRepository.findById(employee.getId());
+
+        assertTrue(optionalEmployee.isPresent());
+
+    }
+
+ /*   @Test(expected = DataAlreadyExistsException.class)
+    public void Should_ThrowException_When_IdAlreadyExists(){
+
+
+    }*/
+
+
 }
