54cfdd17e752c66da79deb9c95d9f158fea7841d
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-04-21 10:45:55 -0500

    Deleting unused import

--- src/main/java/com/example/finalProject/security/BasicAuthConfiguration.java
+++ src/main/java/com/example/finalProject/security/BasicAuthConfiguration.java
@@ -1,17 +1,13 @@
 package com.example.finalProject.security;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,7 +18,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BasicAuthConfiguration {
-
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http,
